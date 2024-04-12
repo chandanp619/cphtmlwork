cd9bc35cd306bc7ec0516538f9c4565fca58f446
chandanp619 <chandanp619@gmail.com>
2024-04-12 12:16:11 +0530

    updated project3

--- project-3/scripts/main.js
+++ project-3/scripts/main.js
@@ -1,4 +1,12 @@
 jQuery( function(){
+
+    jQuery(window).on('scroll', function(){
+        if(jQuery(document).scrollTop() >= 500){
+            jQuery('header').addClass('sticky');
+        }else{
+            jQuery('header').removeClass('sticky');
+        }
+    });
     
     jQuery('.tab-buttons li a').on('click', function(){
         jQuery('.tab-buttons li').removeClass('active');
@@ -29,5 +37,26 @@ jQuery( function(){
             pauseOnHover:false,
             adaptiveHeight: true,
             arrows: false
+        });
+
+        jQuery('input[type="range"]').on('input', function(){
+           // alert(jQuery(this).val());
+          jQuery('.price-range-value').html('0 - '+jQuery(this).val());
+        })
+
+        var pslider = jQuery('#price-slider')[0];
+        noUiSlider.create(pslider, {
+            start: [20000000, 80000000],
+            connect: true,
+            step: 10000,
+            range: {
+                'min': 0,
+                'max': 100000000
+            },
+
+        });
+        pslider.noUiSlider.on('update', function (values, handle) {
+            console.log(handle);
+            jQuery('.price-range-value').html( values[0]+' - '+ values[1]);
         });
 })
